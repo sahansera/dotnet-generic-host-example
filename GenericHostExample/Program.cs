2408c613cbd8cb338c13277a3c4abf26179a96d0
Sahan Serasinghe <sahan.serasinghe@gmail.com>
2020-08-12 23:45:38 +0930

    Init Commit

---
+++ GenericHostExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+
+namespace GenericHostExample
+{
+    public class Program : StartupBase
+    {
+        
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public override void Configure(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+        
+        static void Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureWebHost(webHostBuilder =>
+                {
+                    webHostBuilder
+                        .UseKestrel()
+                        .UseStartup<Program>();
+                })
+                .Build();
+
+            return host.RunAsync();
+        }
+    }
+}
